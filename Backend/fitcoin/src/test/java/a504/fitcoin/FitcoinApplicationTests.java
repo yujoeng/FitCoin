735d9592551180f483ce93f372fb31cff9242955
jalddak <jalddak98@gmail.com>
2026-03-09 20:31:41 +0900

    feat: back-default

---
+++ Backend/fitcoin/src/test/java/a504/fitcoin/FitcoinApplicationTests.java
@@ -0,0 +1,13 @@
+package a504.fitcoin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitcoinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
