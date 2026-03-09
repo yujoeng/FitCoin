ca36c9547e49fdb31b7057e3850633f581e999bd
jalddak <jalddak98@gmail.com>
2026-03-09 21:03:44 +0900

    chore: jenkins test

--- Backend/src/main/java/a504/Backend/BackendApplication.java
+++ Backend/src/main/java/a504/Backend/BackendApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BackendApplication {
+	// jenkins test
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
