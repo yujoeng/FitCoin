b3c511d69388839ed7e9b0dd3865ae1917994497
crhwang822 <crhwang822@gmail.com>
2026-03-11 20:28:47 +0900

    refactor(be): 프로젝트 구조 변경

--- Backend/fitCoin/src/main/java/org/a504/fitCoin/FitCoinApplication.java
+++ Backend/fitCoin/src/main/java/org/a504/fitCoin/FitCoinApplication.java
@@ -1,14 +1,14 @@
-package a504.Backend;
+package org.a504.fitCoin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BackendApplication {
+public class FitCoinApplication {
 	// jenkins test2
 
 	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
+		SpringApplication.run(FitCoinApplication.class, args);
 	}
 
 }
--- Backend/fitCoin/src/test/java/org/a504/fitCoin/FitCoinApplicationTests.java
+++ Backend/fitCoin/src/test/java/org/a504/fitCoin/FitCoinApplicationTests.java
@@ -1,10 +1,10 @@
-package a504.Backend;
+package org.a504.fitCoin;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BackendApplicationTests {
+class FitCoinApplicationTests {
 
 	@Test
 	void contextLoads() {
