15f32a6fb8f070b5ffff99d5b37aa5c83c90ee6d
jalddak <jalddak98@gmail.com>
2026-03-12 22:11:24 +0900

    chore(infra): 프론트 도커파일 수정

--- Frontend/fitCoin/next.config.js
+++ Frontend/fitCoin/next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   // Three.js 등 브라우저 전용 모듈을 위한 webpack 설정
   webpack: (config) => {
     config.externals = [...(config.externals || [])];
