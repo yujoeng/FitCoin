fccb378df50592601040dbef0800debbf1f1ba2b
jameschoi74 <mannerplay24@gmail.com>
2026-03-18 10:31:07 +0900

    fix(be): userid 반영 수정

--- Backend/fitCoin/src/main/java/org/a504/fitCoin/global/config/SecurityConfig.java
+++ Backend/fitCoin/src/main/java/org/a504/fitCoin/global/config/SecurityConfig.java
@@ -28,8 +28,7 @@ public class SecurityConfig {
             "/swagger-ui.html",
             "/v3/api-docs",
             "/v3/api-docs/**",
-            "/auth/**",
-            "/wallet/**"  // TODO: 테스트 완료 후 JWT 인증 구현되면 제거
+            "auth/**"
     };
     private final CorsConfigProperties corsConfigProperties;
 
