ed7bfd303ee42e06c0fd730bbba5186092a525c1
jalddak <jalddak98@gmail.com>
2026-03-19 17:51:14 +0900

    feat(be): asset 시작(-ing)

--- Backend/fitCoin/src/main/java/org/a504/fitCoin/domain/asset/controller/AssetController.java
+++ Backend/fitCoin/src/main/java/org/a504/fitCoin/domain/asset/controller/AssetController.java
@@ -2,7 +2,15 @@ package org.a504.fitCoin.domain.asset.controller;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.a504.fitCoin.domain.asset.dto.AssetResponse;
+import org.a504.fitCoin.domain.auth.security.CustomUserDetails;
+import org.a504.fitCoin.global.response.ApiResponse;
+import org.a504.fitCoin.global.response.status.SuccessStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
@@ -10,4 +18,11 @@ import org.springframework.web.bind.annotation.*;
 @Tag(name = "Asset")
 public class AssetController {
 
+    @GetMapping("/me")
+    public ResponseEntity<ApiResponse<AssetResponse>> myAsset(
+            @AuthenticationPrincipal CustomUserDetails userDetails
+    ) {
+
+        return ApiResponse.onSuccess(SuccessStatus.OK, );
+    }
 }
