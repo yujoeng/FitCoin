d31b4ac950cc24833516cc66b764fef7f74483a3
jameschoi74 <mannerplay24@gmail.com>
2026-03-19 16:45:13 +0900

    docs(be): 캐릭터 도메인 swagger 설명  수정

--- Backend/fitCoin/src/main/java/org/a504/fitCoin/domain/character/controller/CharacterController.java
+++ Backend/fitCoin/src/main/java/org/a504/fitCoin/domain/character/controller/CharacterController.java
@@ -24,7 +24,7 @@ public class CharacterController {
 
     private final CharacterService characterService;
 
-    @Operation(summary = "캐릭터 입양(뽑기)")
+    @Operation(summary = "캐릭터 뽑기", description = "새로운 캐릭터를 데려옵니다.")
     @PostMapping("/adopt")
     public ResponseEntity<ApiResponse<AdoptCharacterResponse>> adoptCharacter(
             @AuthenticationPrincipal CustomUserDetails userDetails
@@ -34,7 +34,7 @@ public class CharacterController {
         return ApiResponse.onSuccess(SuccessStatus.CREATED, response);
     }
 
-    @Operation(summary = "현재 캐릭터 조회")
+    @Operation(summary = "현재 캐릭터 정보 조회", description = "사용자가 현재 성장시키고 있는 캐릭터의 정보(캐릭터 종류, 경험치 등)를 조회합니다.")
     @GetMapping("/me")
     public ResponseEntity<ApiResponse<CharacterResponse>> getMyCharacter(
             @AuthenticationPrincipal CustomUserDetails userDetails
@@ -44,7 +44,7 @@ public class CharacterController {
         return ApiResponse.onSuccess(SuccessStatus.OK, response);
     }
 
-    @Operation(summary = "캐릭터 졸업")
+    @Operation(summary = "캐릭터 졸업", description = "현재 캐릭터가 졸업이 가능한 상태일 경우, 졸업 로직을 트리거합니다. 기프티콘을 지급합니다. 캐릭터를 도감에 추가합니다.")
     @PatchMapping("/graduate")
     public ResponseEntity<ApiResponse<Void>> graduateCharacter(
             @AuthenticationPrincipal CustomUserDetails userDetails
@@ -54,7 +54,7 @@ public class CharacterController {
         return ApiResponse.onSuccess(SuccessStatus.OK);
     }
 
-    @Operation(summary = "캐릭터 도감 조회")
+    @Operation(summary = "캐릭터 도감 조회", description = "캐릭터 도감의 상태를 조회합니다.")
     @GetMapping("/dex")
     public ResponseEntity<ApiResponse<List<CharacterDexResponse>>> getCharacterDex(
             @AuthenticationPrincipal CustomUserDetails userDetails
