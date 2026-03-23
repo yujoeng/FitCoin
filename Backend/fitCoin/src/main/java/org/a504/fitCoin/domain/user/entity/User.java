25dcd8b1aeb2afd73ec63550eeb3568f1879f2cd
jalddak <jalddak98@gmail.com>
2026-03-23 20:33:59 +0900

    fix(be): 닉네임 중복 허용 / db 유니크 제약도 해제함

--- Backend/fitCoin/src/main/java/org/a504/fitCoin/domain/user/entity/User.java
+++ Backend/fitCoin/src/main/java/org/a504/fitCoin/domain/user/entity/User.java
@@ -18,7 +18,7 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, unique = true)
+    @Column(nullable = false)
     private String email;
 
     @Column(nullable = false)
